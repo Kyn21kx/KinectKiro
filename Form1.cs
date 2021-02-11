02f04ab97c66627219ee71ed9dfeb23f74605176
Nef <nefesmeralda@gmail.com>
2021-02-11 04:02:38 -0600

    First actual commit

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+
+namespace KinectSekiro {
+    public partial class Form1 : Form {
+
+        private KinectSensor sensor;
+        
+        public Form1() {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start() {
+            sensor = KinectSensor.GetDefault();
+        }
+
+        public void Stream() {
+            if(sensor != null) {
+                sensor.Open();
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            sensor.Close();
+        }
+    }
+}
